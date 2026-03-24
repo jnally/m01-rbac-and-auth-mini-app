578237d67bfe3d16b7b38b0ea10714c1ee342c24
Jeremy Nally <jnally3@ivytech.edu>
2026-03-23 20:58:34 -0400

    refactor code. remove duplications. streamline user experience more.

--- login_simulation.py
+++ login_simulation.py
@@ -1,6 +1,118 @@
 ## Author: Jeremy Nally
 ## Course: SDEV245 - Security and Secure Coding
-##
+
+# users dictionary with roles
+users = {
+    'carl': {'role':'admin'},
+    'donut': {'role':'user'}
+    }
+
+# simple math test questions for use by the functions
+test_questions = {
+    1: {'question':'What is 4*5?','answer':20},
+    2: {'question':'What is 2^6?','answer':64},
+    3: {'question':'What is 5!?','answer':120},
+    4: {'question':'What is the slope of a horizontal line?','answer':0},
+    }
+
+# presents the answer key to the math test (admin only!)
+def view_answer_key():
+    print('\nTest Answer Key (keep secret)\n')
+    for i in test_questions:
+        print(i, ': question: ', test_questions[i]['question'], ' answer: ', test_questions[i]['answer'],sep='')
+
+# administers math test (user role only!)
+def take_test():
+    print('\nMath Test')
+    score = 0
+    possible = 100
+    for i in test_questions:
+        question = test_questions[i]['question']
+        question = 'Question #' + str(i) + ': ' + question + ' Entry: '
+        answer = test_questions[i]['answer']
+        user_input = input(question)
+        while not(user_input.isnumeric()):
+            print('Invalid Input: Enter a number.')
+            user_input = input(question)
+        if int(user_input) == answer:
+            score += 25
+    print('\nYour test score is ', score, ' out of ', possible, '.', sep='')
+
+# verify that username is in the dictionary
+def username_exists(username):
+    if username in users:
+        return True
+    return False
+
+# present very trusting user login, admin carl can view math test answer key, user donut can take a math test
+def main():
+    keep_going = True
+    logged_in = False
+    username = ''
+    attempts = 0
+    choice = ''
+
+    # loop until user wishes to exit or too many failed login attempts
+    while keep_going:
+        # let user log in with just username  (up to 5 attempts)
+        if not(logged_in):
+            username = input('\nEnter your username (Q to quit): ')
+            if username.upper() == 'Q':
+                print('Quitting...')
+                return 0
+            attempts += 1
+            while not(username_exists(username)):
+                if attempts >= 5:
+                    print('Too many login attempts. Exiting...')
+                    return 1
+                print('Invalid username')
+                username = input('Enter your username (Q to quit): ')
+                if username.upper() == 'Q':
+                    print('Quitting...')
+                    return 0
+                attempts += 1
+            logged_in = True
+            attempts = 0
+        if not(logged_in):
+            username = ''
+            print('Exiting...')
+            return 1
+        user = users[username]
+
+        # menu
+        if user['role'] == 'admin': # admin role only
+            print('\nAdmin Menu')
+            print('View Math Test Answer Key. Enter K')
+            print('Logout. Enter L')
+            print('Quit Program. Enter Q')
+            choice = input('Enter selection: ')
+            if choice.upper() == 'K':
+                view_answer_key()
+        elif user['role'] == 'user': # user role only 
+            print('\nUser Menu')
+            print('Take Math Test. Enter T')
+            print('Logout. Enter L')
+            print('Quit Program. Enter Q')
+            choice = input('Enter selection: ')
+            if choice.upper() == 'T':
+                take_test()
+        # logic for rest of the menu that works for both roles
+        if choice.upper() == 'L':
+                print(username,'has logged out.')
+                username = ''
+                logged_in = False
+        elif choice.upper() == 'Q':
+                print('Quitting...')
+                username = ''
+                logged_in = False
+                keep_going = False
+        # if user does not enter a valid choice, it will loop around
+    return 0
+
+# execute main function
+if __name__ == '__main__':
+    main()
+
 ## Assignment Instructions (modified for relevant details):
 ##
 ## Module 1: Assignment - RBAC and Authentication Mini-App
@@ -83,131 +195,5 @@
 ## Clean, readable, and well-documented
 ## 10 pts
 ##
-
-# users dictionary with roles
-users = {
-    'carl': {'role':'admin'},
-    'donut': {'role':'user'}
-    }
-
-# simple math test questions for use by the functions
-test_questions = {
-    1: {'question':'What is 4*5?','answer':20},
-    2: {'question':'What is 2^6?','answer':64},
-    3: {'question':'What is 5!?','answer':120},
-    4: {'question':'What is the slope of a horizontal line?','answer':0},
-    }
-
-# presents the answer key to the math test (admin only!)
-def view_answer_key():
-    print('\nTest Answer Key (keep secret)\n')
-    for i in test_questions:
-        print(i, ': question: ', test_questions[i]['question'], ' answer: ', test_questions[i]['answer'],sep='')
-
-# administers math test (user role only!)
-def take_test():
-    print('\nMath Test')
-    score = 0
-    possible = 100
-    for i in test_questions:
-        question = test_questions[i]['question']
-        question = 'Question #' + str(i) + ': ' + question + ' Entry: '
-        answer = test_questions[i]['answer']
-        user_input = input(question)
-        while not(user_input.isnumeric()):
-            print('Invalid Input: Enter a number.')
-            user_input = input(question)
-        if int(user_input) == answer:
-            score += 25
-    print('\nYour test score is ', score, ' out of ', possible, '.', sep='')
-
-# verify that username is in the dictionary
-def username_exists(username):
-    if username in users:
-        return True
-    return False
-
-# very trusting login function that only prompts for a username and no password
-def login():
-    username = input('Enter your username: ')
-    while not(username_exists):
-        print('Invalid username')
-        username = input('Enter your username: ')    
-    return username
-
-# present very trusting user login, admin carl can view math test answer key, user donut can take a math test
-def main():
-    keep_going = True
-    logged_in = False
-    username = ''
-    attempts = 0
-
-    # loop until user wishes to exit or too many failed login attempts
-    while keep_going:
-        # let user log in with just username  (up to 5 attempts)
-        if not(logged_in):
-            username = input('\nEnter your username: ')
-            attempts += 1
-            while not(username_exists(username)):
-                if attempts >= 5:
-                    print('Too many login attempts. Exiting...')
-                    return 1
-                print('Invalid username')
-                username = input('Enter your username: ')
-                attempts += 1
-            logged_in = True
-            attempts = 0
-        if not(logged_in):
-            username = ''
-            del user
-            print('Exiting...')
-            return 1
-        user = users[username]
-
-        # menu
-        if user['role'] == 'admin':
-            print('\nAdmin Menu')
-            print('View Math Test Answer Key. Enter K')
-            print('Logout. Enter L')
-            print('Quit Program. Enter Q')
-            choice = input('Enter selection: ')
-            if choice.upper() == 'K':
-                view_answer_key()
-            elif choice.upper() == 'L':
-                username = ''
-                del user
-                logged_in = False
-            elif choice.upper() == 'Q':
-                username = ''
-                del user
-                logged_in = False
-                keep_going = False
-        elif user['role'] == 'user':
-            print('\nUser Menu')
-            print('Take Math Test. Enter T')
-            print('Logout. Enter L')
-            print('Quit Program. Enter Q')
-            choice = input('Enter selection: ')
-            if choice.upper() == 'T':
-                take_test()
-            elif choice.upper() == 'L':
-                username = ''
-                del user
-                logged_in = False
-            elif choice.upper() == 'Q':
-                username = ''
-                del user
-                logged_in = False
-                keep_going = False
-        else:
-            print('Nothing to choose...') # should not happen with current users
-            logged_in = False
-            keep_going = False
-    return 0
-
-if __name__ == '__main__':
-    main()
-
-
 
 
